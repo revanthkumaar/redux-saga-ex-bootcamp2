4bc55a6b2cc9e63b437a5773bae2bb5d3fe7931d
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-23 18:51:19 +0530

    d
    asdas

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//dependencies for redux and redux saga
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
+import logger from 'redux-logger'
+
+
+//setup saga - creating instance of saga in react app
+const sagaMiddleware = createSagaMiddleware()
+
+let store = createStore(reducer,applyMiddleware(sagaMiddleware,logger))
+
+sagaMiddleware.run()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
